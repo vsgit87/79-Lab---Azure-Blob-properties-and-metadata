4bc8af80940e84fc00a9cb90922f7e39f30def3f
vignesh <vigneshsmit@gmail.com>
2020-12-15 15:19:50 +1300

    eighteenth

---
+++ blobproperties/blob/Program.cs
@@ -0,0 +1,83 @@
+﻿using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace blobproperties
+{
+    class Program
+    {
+        static string connstring = "DefaultEndpointsProtocol=https;AccountName=demostore9090;AccountKey=qHParFLcMnKcdzqOa132Yu6gkTmXbpTgJBfOk2zOMXUcpaRD8NRY+Ammnof3iddGyyWmzqiuNk/9oFJWAhPoZQ==;EndpointSuffix=core.windows.net";
+        static string containerName = "data";
+        static string filename = "sample.txt";
+        static async Task Main(string[] args)
+        {
+            GetProperties();
+            SetMetadata();
+            GetMetadata();
+            
+            Console.WriteLine("Operation complete");
+            Console.ReadKey();
+        }
+
+        static void GetProperties()
+        {
+            
+            BlobServiceClient blobServiceClient = new BlobServiceClient(connstring);
+            
+            BlobContainerClient containerClient = blobServiceClient.GetBlobContainerClient(containerName);
+            
+            BlobClient blob = containerClient.GetBlobClient(filename);
+
+            
+            BlobProperties properties=blob.GetProperties();
+            Console.WriteLine("The Access tier of the blob is {0}",properties.AccessTier);
+            Console.WriteLine("The Content Length of the blob is {0}", properties.ContentLength);
+
+            
+        }
+
+        static void GetMetadata()
+        {
+            
+            BlobServiceClient blobServiceClient = new BlobServiceClient(connstring);
+            
+            
+            BlobContainerClient containerClient = blobServiceClient.GetBlobContainerClient(containerName);
+            
+            BlobClient blob = containerClient.GetBlobClient(filename);
+
+            
+            BlobProperties properties = blob.GetProperties();
+
+            
+            foreach(var metadata in properties.Metadata)
+            {
+                Console.WriteLine(metadata.Key.ToString());
+                Console.WriteLine(metadata.Value.ToString());
+            }
+        }
+
+        static void SetMetadata()
+        {
+            string p_key = "ApplicationType";
+            string p_value = "Ecommerce";
+
+            BlobServiceClient blobServiceClient = new BlobServiceClient(connstring);
+            
+            BlobContainerClient containerClient = blobServiceClient.GetBlobContainerClient(containerName);
+            
+            BlobClient blob = containerClient.GetBlobClient(filename);
+               
+            IDictionary<string, string> obj = new Dictionary<string, string>();
+            obj.Add(p_key, p_value);
+            blob.SetMetadata(obj);
+            
+
+
+        }
+
+    }
+}
